afe61a4714f881bb255c69cf8da2cc42a74cd73e
nedas60 <nedas60@gmail.com>
2022-03-08 09:51:43 +0200

    Fixed lighting rotation and inverted normals

--- N-Body simulation/src/Assets/Shaders/Fragment.glsl
+++ N-Body simulation/src/Assets/Shaders/Fragment.glsl
@@ -5,14 +5,17 @@ in vec3 color;
 out vec4 result;
 
 void main() {
-	vec3 lightPos = vec3(100,100,100);
+	
+	vec3 ambient = vec3(0.05, 0.05, 0.05);
+	vec3 normalnorm = normalize(normal);
+	vec3 lightPos = vec3(150,150,150);
 	vec3 lightdir = normalize(lightPos - pos);  
 
 	vec3 lightColor = vec3(1.0, 1.0, 1.0);
 	vec3 objectColor = color;
 
-	float diff = max(dot(normal, lightdir), 0.0);
+	float diff = max(dot(normalnorm, lightdir), 0.0);
 	vec3 diffuse = diff * lightColor;
 
-	result = vec4((diffuse * objectColor), 1.0);
+	result = vec4(((ambient + diffuse) * objectColor), 1.0);
 }
